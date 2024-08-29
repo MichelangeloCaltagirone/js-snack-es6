5249c36c7a35ae24e4bca7a02c92019861189af9
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-08-29 16:47:48 +0200

    Snak2 Completed. Esercizio Finito

--- js/script.js
+++ js/script.js
@@ -30,9 +30,10 @@ Id  Name                Grades
 
 
 
-// Snak1
+// Snack1
 
 //Prep
+// TODO farlo con .reduce
 
 // Variabili note
 const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
@@ -54,4 +55,90 @@ invited.forEach((g, i) => {
     listXtypography.push(guestListed);
 });
 // Mostro l'output in console
-console.log(listXtypography);
+console.log('La lista per la tipografia è la seguente' ,listXtypography);
+
+
+
+// Snack2
+
+/**
+ * Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna 	    48
+145 Gaia Borromini          74
+196 Luigi Grimaldello 	    68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84
+ */
+
+// Prep 
+
+// Variabili note
+const students = [
+                  {
+                    id: 213,
+                    name: 'Marco della Rovere',
+                    grades: 78
+                  },
+                  {
+                    id: 110,
+                    name: 'Paola Cortellessa',
+                    grades: 96
+                  },
+                  {
+                    id: 250,
+                    name: 'Andrea Mantegna',
+                    grades: 48
+                  },
+                  {
+                    id: 145,
+                    name: 'Gaia Borromini',
+                    grades: 74
+                  },
+                  {
+                    id:196,
+                    name: 'Luigi Grimaldello',
+                    grades: 68
+                  },
+                  {
+                    id: 102,
+                    name: 'Piero della Francesca',
+                    grades: 50
+                  },
+                  {
+                    id: 120,
+                    name: 'Francesca da Polenta',
+                    grades: 84
+                  },
+];
+
+// Fase di elaborazione
+
+// preparo la variabile per l'output. Array che conterrà una lista di soli nomi tutti maiuscoli
+let newClassNames = [];
+
+// giro tutti gli elementi dell'array noto, ne estrapolo il nome dal valore della key, e lo aggiungo all'array per l'output
+students.forEach(s => newClassNames.push(s.name.toUpperCase()));
+
+// Fase di output in console
+console.log('La lista di nomi per la nuova classe è:', newClassNames);
+
+
+// Certo chi ha una somma dei voti maggiore a 70
+const grades70High = students.filter(s => {
+    if(s.grades > 70) return true;
+    else return false
+});
+
+// Mostro output
+console.log('Questi sono gli studenti con una somma di voti superiore a 70', grades70High);
+
+// Cerco chi ha  una somma di voti superiore a 70 e id superiore a 120
+const id120PlusGrades = students.filter(s => {
+    if(s.grades > 70 && s.id > 120) return true;
+    else return false; 
+});
+
+// mostro output
+console.log('Lista degli studenti con somma voti maggiore di 70 e id maggiore di 120', id120PlusGrades);
