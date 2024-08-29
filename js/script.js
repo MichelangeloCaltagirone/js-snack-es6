c48961ccf5c8235f393215b94b6c4f3ef8659472
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-08-29 18:00:08 +0200

    Refactoring done. Better understanding of the synthax of varius methods. Really helpfull.

--- js/script.js
+++ js/script.js
@@ -33,16 +33,29 @@ Id  Name                Grades
 // Snack1
 
 //Prep
-// TODO farlo con .reduce
+
 
 // Variabili note
 const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
+// Col metoro reduce, creo un elemento che desidero e lo popolo basandomi sugli elementi di un array, manipolandoli
+const listXtypography = invited.reduce((acc, g, i) => {
+    guestListed = {
+        // creo un oggetto rappresentante l'elemento con le seguenti proprietà:
+                    tableName: 'Tavolo Vip',
+                    guestName: g,
+                    place: i + 1
+                  };
+    acc.push(guestListed);
+    return acc;
+}, []);
+
 // Dichiaro e inizzializzo la variabile per l'output
-let listXtypography = [];
+//let listXtypography = [];
 
 // Fase di Elaborazione
 // per ogni elemento nell'array invited
+/*
 invited.forEach((g, i) => {
     //prendo l'elemento stesso(g), e il suo indice(i) dai parametri della funzione di callBack
     guestListed = {
@@ -55,22 +68,12 @@ invited.forEach((g, i) => {
     listXtypography.push(guestListed);
 });
 // Mostro l'output in console
+*/
 console.log('La lista per la tipografia è la seguente' ,listXtypography);
 
 
 
 // Snack2
-
-/**
- * Id  Name                Grades
-213 Marco della Rovere      78
-110 Paola Cortellessa       96
-250 Andrea Mantegna 	    48
-145 Gaia Borromini          74
-196 Luigi Grimaldello 	    68
-102 Piero della Francesca   50
-120 Francesca da Polenta    84
- */
 
 // Prep 
 
@@ -115,30 +118,39 @@ const students = [
 
 // Fase di elaborazione
 
-// preparo la variabile per l'output. Array che conterrà una lista di soli nomi tutti maiuscoli
-let newClassNames = [];
+// Creo un nuovo array basandomi sugli elementi(oggetti) di un altro array, andando a prelevare il valore di una specifica key
+//const newClassNames = students.map(s => s.name);
 
-// giro tutti gli elementi dell'array noto, ne estrapolo il nome dal valore della key, e lo aggiungo all'array per l'output
-students.forEach(s => newClassNames.push(s.name.toUpperCase()));
+// Cerco chi ha una somma dei voti maggiore a 70
+//const grades70High = students.filter(s => s.grades > 70);
 
-// Fase di output in console
-console.log('La lista di nomi per la nuova classe è:', newClassNames);
+// Cerco chi ha una somma di voti superiore a 70 e id superiore a 120
+//const id120PlusGrades = students.filter(s => s.grades > 70 && s.id > 120);
+
+// Fase di Output (in console)
+//console.log('La lista di nomi per la nuova classe è:', students.map(s => s.name.toUpperCase()));
+//console.log('Questi sono gli studenti con una somma di voti superiore a 70', students.filter(s => s.grades > 70));
+//console.log('Lista degli studenti con somma voti maggiore di 70 e id maggiore di 120', students.filter(s => s.grades > 70 && s.id > 120));
 
 
-// Certo chi ha una somma dei voti maggiore a 70
-const grades70High = students.filter(s => {
-    if(s.grades > 70) return true;
-    else return false
-});
 
-// Mostro output
-console.log('Questi sono gli studenti con una somma di voti superiore a 70', grades70High);
+// Versione col metodo .reduce()
 
-// Cerco chi ha  una somma di voti superiore a 70 e id superiore a 120
-const id120PlusGrades = students.filter(s => {
-    if(s.grades > 70 && s.id > 120) return true;
-    else return false; 
-});
+const listsStudents = students.reduce((acc, s) => {
+    acc[0].push(s.name.toUpperCase());
+    if (s.grades > 70) acc[1].push(s);
+    if (s.grades > 70 && s.id > 120) acc[2].push(s);
+    return acc;
+}, [[], [], []])
 
-// mostro output
-console.log('Lista degli studenti con somma voti maggiore di 70 e id maggiore di 120', id120PlusGrades);
+console.log('La lista di nomi per la nuova classe è:', listsStudents[0]);
+console.log('Questi sono gli studenti con una somma di voti superiore a 70', listsStudents[1]);
+console.log('Lista degli studenti con somma voti maggiore di 70 e id maggiore di 120', listsStudents[2]);
+
+
+console.log(students.reduce((acc, s) => {
+    acc[0].push(s.name.toUpperCase());
+    if (s.grades > 70) acc[1].push(s);
+    if (s.grades > 70 && s.id > 120) acc[2].push(s);
+    return acc;
+}, [[], [], []]));
